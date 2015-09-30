1ee18dacc6b7aea1228980570effc8b82b3232cd
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2015-09-30 18:53:53 +0200

    first expectation

--- R/test_getting_started.R
+++ R/test_getting_started.R
@@ -2,6 +2,7 @@
 test_getting_started <- function(skip = NULL, ctx = get_default_context()) {
   tests <- list(
     package_dependencies = function() {
+      expect_true(TRUE)
     }
   )
   run_tests(tests, skip)
