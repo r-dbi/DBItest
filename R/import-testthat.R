8981a21efaa90ca2dfedce5353c612b1f507510a
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2015-09-30 22:30:26 +0200

    upgrade testthat

--- R/import-testthat.R
+++ R/import-testthat.R
@@ -15,8 +15,8 @@
 #' @importFrom testthat expect_equivalent expect_error expect_false
 #' @importFrom testthat expect_identical expect_is expect_less_than
 #' @importFrom testthat expect_match expect_message expect_more_than
-#' @importFrom testthat expect_named expect_null expect_output expect_that
-#' @importFrom testthat expect_true expect_warning
+#' @importFrom testthat expect_named expect_null expect_output expect_silent
+#' @importFrom testthat expect_that expect_true expect_warning
 #' @importFrom testthat fail find_test_scripts
 #' @importFrom testthat get_reporter gives_warning
 #' @importFrom testthat has_names
@@ -30,12 +30,11 @@
 #' @importFrom testthat prints_text
 #' @importFrom testthat Reporter RstudioReporter
 #' @importFrom testthat set_reporter shows_message skip skip_if_not_installed
-#' @importFrom testthat skip_on_cran skip_on_travis source_dir
-#' @importFrom testthat source_test_helpers succeed
+#' @importFrom testthat skip_on_appveyor skip_on_cran skip_on_os skip_on_travis
+#' @importFrom testthat source_dir source_test_helpers succeed
 #' @importFrom testthat SilentReporter StopReporter SummaryReporter
 #' @importFrom testthat takes_less_than test_check test_dir test_env
 #' @importFrom testthat test_example test_examples test_file test_package
 #' @importFrom testthat test_that throws_error
 #' @importFrom testthat TapReporter TeamcityReporter
 #' @importFrom testthat watch with_mock with_reporter
-NULL
