4bbb9b8c190700e6d3083417db7d7f3de33fbb68
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2015-09-30 19:03:29 +0200

    test if context is available

--- R/test_getting_started.R
+++ R/test_getting_started.R
@@ -1,6 +1,11 @@
 #' @export
 test_getting_started <- function(skip = NULL, ctx = get_default_context()) {
   tests <- list(
+    has_context = function() {
+      expect_true(!is.null(ctx))
+      expect_true(!is.null(ctx$drv))
+    },
+
     package_dependencies = function() {
       expect_true(TRUE)
     }
