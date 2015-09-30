ae4fa7d792e94175d261ace0fe3c2866a2f9087a
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2015-09-30 22:37:52 +0200

    positive tests, strip info for now

--- R/test_getting_started.R
+++ R/test_getting_started.R
@@ -4,30 +4,20 @@ test_getting_started <- function(skip = NULL, ctx = get_default_context()) {
 
   tests <- list(
     has_context = function() {
-      expect_true(
-        !is.null(ctx),
-        info = "Please call make_context() before using any of the tests in DBItest.")
-      expect_true(!is.null(ctx$drv))
+      expect_is(ctx, "DBItest_context")
+      expect_is(ctx$drv, "DBIDriver")
     },
 
     package_dependencies = function() {
-      pkg_name <- attr(class(ctx$drv), "package")
+      expect_is(pkg_name <- attr(class(ctx$drv), "package"), "character")
 
-      expect_that(
-        pkg_path <- find.package(pkg_name), not(throws_error()),
-        info = "Please ensure that your driver is an S4 class defined in your package.")
+      pkg_path <- find.package(pkg_name)
 
       pkg <- devtools::as.package(pkg_path)
-      depends <- devtools::parse_deps(pkg$depends)
-      expect_true(
-        "DBI" %in% depends$name,
-        info = "Please include DBI in the 'Depends' section of your DESCRIPTION."
-      )
-      imports <- devtools::parse_deps(pkg$imports)
-      expect_true(
-        "methods" %in% imports$name,
-        info = "Please include methods in the 'Imports' section of your DESCRIPTION."
-      )
+      pkg_depends <- devtools::parse_deps(pkg$depends)$name
+      expect_true("DBI" %in% pkg_depends)
+      pkg_imports <- devtools::parse_deps(pkg$imports)$name
+      expect_true("methods" %in% pkg_imports)
     }
   )
   run_tests(tests, skip, test_suite)
