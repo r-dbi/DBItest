cc9134f380fc5e1e1f50073d0906db6ca2ba68a9
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2015-09-30 22:21:04 +0200

    package_dependencies test

--- R/test_getting_started.R
+++ R/test_getting_started.R
@@ -9,7 +9,23 @@ test_getting_started <- function(skip = NULL, ctx = get_default_context()) {
     },
 
     package_dependencies = function() {
-      expect_true(TRUE)
+      pkg_name <- attr(class(ctx$drv), "package")
+
+      expect_that(
+        pkg_path <- find.package(pkg_name), not(throws_error()),
+        info = "Please ensure that your driver is an S4 class defined in your package.")
+
+      pkg <- devtools::as.package(pkg_path)
+      depends <- devtools::parse_deps(pkg$depends)
+      expect_true(
+        "DBI" %in% depends$name,
+        info = "Please include DBI in the 'Depends' section of your DESCRIPTION."
+      )
+      imports <- devtools::parse_deps(pkg$imports)
+      expect_true(
+        "methods" %in% imports$name,
+        info = "Please include methods in the 'Imports' section of your DESCRIPTION."
+      )
     }
   )
   run_tests(tests, skip)
