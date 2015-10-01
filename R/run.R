65c12e92707addc7fbd66382823b8bf4030fd987
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2015-10-01 20:40:58 +0200

    call skip() for skipped tests

--- R/run.R
+++ R/run.R
@@ -1,10 +1,12 @@
 run_tests <- function(tests, skip, test_suite) {
-  tests <- tests[!names(tests) %in% skip]
-
   context(paste("DBItest:", test_suite))
   lapply(names(tests), function(test_name) {
     test_that(paste0("DBItest: ", test_name), {
-      tests[[test_name]]()
+      if (test_name %in% skip) {
+        skip("by request")
+      } else {
+        tests[[test_name]]()
+      }
     })
   })
 }
