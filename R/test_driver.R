0177743d646910d5c66355dce1572815197ecde8
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2015-10-01 21:42:33 +0200

    add question

--- R/test_driver.R
+++ R/test_driver.R
@@ -12,6 +12,14 @@ test_driver <- function(skip = NULL, ctx = get_default_context()) {
       expect_is(ctx$drv, "DBIDriver")
     },
 
+    # dbGetInfo contains necessary elements
+    # Q: dbGetInfo for Driver has been removed everywhere except for RSQLite
+    #   (where it returns an empty list). dbGetInfo for other objects
+    #   is deprecated in RSQLite, but available in other backends. How to
+    #   resolve?
+
+
+
     NULL
   )
   run_tests(tests, skip, test_suite)
