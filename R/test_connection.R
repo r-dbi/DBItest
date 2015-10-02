8049bfdc13bad1e1067c0b0692bc884a05765556
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2015-10-02 22:13:29 +0200

    stress tests

--- R/test_connection.R
+++ R/test_connection.R
@@ -7,16 +7,30 @@ test_connection <- function(skip = NULL, ctx = get_default_context()) {
   test_suite <- "Connection"
 
   tests <- list(
-    # Can connect and disconnect
+    # Can connect and disconnect, connection object inherits from
+    #   "DBIConnection"
     can_connect_and_disconnect = function() {
       con <- connect(ctx)
       expect_is(con, "DBIConnection")
       expect_success(dbDisconnect(con))
     },
 
-    # Connection inherits from "DBIConnection" class
-    inherits_from_connection = function() {
-      #expect_is(ctx$drv, "DBIDriver")
+    # Open 100 simultaneous connections
+    simultaneous_connections = function() {
+      cons <- lapply(seq_len(100L), function(i) connect(ctx))
+      inherit_from_connection <-
+        vapply(cons, inherits, what = "DBIConnection", logical(1))
+      expect_true(all(inherit_from_connection))
+      expect_success(lapply(cons, dbDisconnect))
+    },
+
+    # Open and close 100 connections
+    stress_connections = function() {
+      for (i in seq_len(100L)) {
+        con <- connect(ctx)
+        expect_is(con, "DBIConnection")
+        expect_success(dbDisconnect(con))
+      }
     },
 
     NULL
