877d035f071e2e438ac94a352629b50f4a6100db
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2015-10-11 11:00:53 +0200

    quote strings

--- R/test_sql.R
+++ R/test_sql.R
@@ -7,12 +7,25 @@ test_sql <- function(skip = NULL, ctx = get_default_context()) {
   test_suite <- "SQL"
 
   tests <- list(
-    # Can issue trivial query, result object inherits from "DBIResult"
-    trivial_query = function() {
+    # Can quote strings
+    quote_string = function() {
       with_connection({
-        res <- dbSendQuery(con, "SELECT 1")
-        on.exit(dbClearResult(res), add = TRUE)
-        expect_is(res, "DBIResult")
+        simple <- dbQuoteString(con, "simple")
+        with_spaces <- dbQuoteString(con, "with spaces")
+        quoted_simple <- dbQuoteString(con, as.character(simple))
+        quoted_with_spaces <- dbQuoteString(con, as.character(with_spaces))
+
+        query <- paste0("SELECT",
+                        simple, "as simple,",
+                        with_spaces, "as with_spaces,",
+                        quoted_simple, "as quoted_simple,",
+                        quoted_with_spaces, "as quoted_with_spaces")
+
+        expect_warning(rows <- dbGetQuery(con, query), NA)
+        expect_equal(rows$simple, "simple")
+        expect_equal(rows$with_spaces, "with spaces")
+        expect_equal(rows$quoted_simple, as.character(simple))
+        expect_equal(rows$quoted_with_spaces, as.character(with_spaces))
       })
     },
 
