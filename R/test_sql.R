b821f48d6d8a993828f7f2471af0af2c4590e401
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2015-11-07 00:39:21 +0100

    test date roundtrip

--- R/test_sql.R
+++ R/test_sql.R
@@ -239,6 +239,21 @@ test_sql <- function(skip = NULL, ctx = get_default_context()) {
       })
     },
 
+    #' \item{\code{roundtrip_date}}{
+    #' Can create tables with date values.
+    #' }
+    roundtrip_date = function() {
+      with_connection({
+        tbl_in <- data.frame(a = Sys.Date() + 1:5)
+
+        on.exit(dbRemoveTable(con, "test"), add = TRUE)
+        dbWriteTable(con, "test", tbl_in)
+
+        tbl_out <- dbReadTable(con, "test")
+        expect_equal(tbl_in, tbl_out)
+      })
+    },
+
     NULL
   )
   #' }
