cd5faa628e235ffcf2e134075bc43c4239392351
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2015-11-12 22:52:14 +0100

    Test `NA` to `NULL` conversion in `dbQuoteString()`, and false friends (#23)
    Fixes #23.

--- R/test_sql.R
+++ R/test_sql.R
@@ -26,18 +26,30 @@ test_sql <- function(skip = NULL, ctx = get_default_context()) {
         with_spaces <- dbQuoteString(con, "with spaces")
         quoted_simple <- dbQuoteString(con, as.character(simple))
         quoted_with_spaces <- dbQuoteString(con, as.character(with_spaces))
+        null <- dbQuoteString(con, NA_character_)
+        quoted_null <- dbQuoteString(con, as.character(null))
+        na <- dbQuoteString(con, "NA")
+        quoted_na <- dbQuoteString(con, as.character(na))
 
         query <- paste0("SELECT",
                         simple, "as simple,",
                         with_spaces, "as with_spaces,",
+                        null, " as null_return,",
+                        na, "as na_return,",
                         quoted_simple, "as quoted_simple,",
-                        quoted_with_spaces, "as quoted_with_spaces")
+                        quoted_with_spaces, "as quoted_with_spaces,",
+                        quoted_null, "as quoted_null,",
+                        quoted_na, "as quoted_na")
 
         expect_warning(rows <- dbGetQuery(con, query), NA)
         expect_equal(rows$simple, "simple")
         expect_equal(rows$with_spaces, "with spaces")
+        expect_true(is.na(rows$null))
+        expect_equal(rows$na_return, "NA")
         expect_equal(rows$quoted_simple, as.character(simple))
         expect_equal(rows$quoted_with_spaces, as.character(with_spaces))
+        expect_equal(rows$quoted_null, as.character(null))
+        expect_equal(rows$quoted_na, as.character(na))
       })
     },
 
