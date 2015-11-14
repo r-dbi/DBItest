72a5ab09acb4f8a84998d94f800cdc8d78ba115a
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2015-11-14 23:12:46 +0100

    Add package documentation (#38)
    Fixes #38.

---
+++ R/DBItest.R
@@ -0,0 +1,10 @@
+#' @details
+#' The two most important functions are \code{\link{make_context}} and
+#' \code{\link{test_all}}.  The former tells the package how to connect to your
+#' DBI backend, the latter executes all tests of the test suite. More
+#' fine-grained test functions (all with prefix \code{test_}) are available.
+#'
+#' See the package's vignette for more details.
+#'
+#' @author Kirill Müller
+"_PACKAGE"
