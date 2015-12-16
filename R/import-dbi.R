2d96df134974531baceb1b717455c3f8b42ad9c7
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2015-12-17 00:30:47 +0100

    remove unneeded imports

--- R/import-dbi.R
+++ R/import-dbi.R
@@ -1,7 +1,5 @@
 # The imports below were generated using the following call:
 # @import.gen::importFrom("DBI")
-#' @importFrom DBI ANSI
-#' @importFrom DBI columnToRownames
 #' @importFrom DBI dbBegin dbBind dbCallProc dbClearResult dbColumnInfo
 #' @importFrom DBI dbCommit dbConnect dbDataType dbDisconnect dbDriver
 #' @importFrom DBI dbExistsTable dbFetch dbGetDBIVersion dbGetException
@@ -11,14 +9,4 @@
 #' @importFrom DBI dbQuoteIdentifier dbQuoteString dbReadTable dbRemoveTable
 #' @importFrom DBI dbRollback dbSendQuery dbSetDataMappings dbUnloadDriver
 #' @importFrom DBI dbWriteTable
-#' @importFrom DBI fetch
-#' @importFrom DBI isSQLKeyword isSQLKeyword.default
-#' @importFrom DBI make.db.names make.db.names.default
-#' @importFrom DBI print.list.pairs
-#' @importFrom DBI rownamesToColumn
-#' @importFrom DBI show sqlAppendTable sqlAppendTableTemplate sqlCommentSpec
-#' @importFrom DBI sqlCreateTable sqlData sqlInterpolate sqlParseVariables
-#' @importFrom DBI sqlParseVariablesImpl sqlQuoteSpec
-#' @importFrom DBI SQL SQLKeywords
-#' @importFrom DBI Table
 NULL
