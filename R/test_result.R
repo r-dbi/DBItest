d492e00b48f3858cab046b7d9f6976dd9f1d66e3
Marcel Boldt <boldt@live.de>
2016-01-09 19:13:32 +0100

    trivial_test: now expect TRUE from dbClearResult

--- R/test_result.R
+++ R/test_result.R
@@ -23,7 +23,7 @@ test_result <- function(skip = NULL, ctx = get_default_context()) {
     trivial_query = function() {
       with_connection({
         res <- dbSendQuery(con, "SELECT 1")
-        on.exit(expect_error(dbClearResult(res), NA), add = TRUE)
+        on.exit(expect_true(dbClearResult(res), NA), add = TRUE)
         expect_is(res, "DBIResult")
       })
     },
