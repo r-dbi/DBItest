33d4638486f414eeffd593a4904d3fc1ed5cfe0f
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2016-03-10 09:38:58 +0100

    compat

--- R/expectations.R
+++ R/expectations.R
@@ -1,9 +1,8 @@
 arglist_is_empty <- function() {
   function(x) {
-    expectation(
+    expect_true(
       is.null(formals(x)),
-      "has empty argument list",
-      "has at least one argument")
+      "has empty argument list")
   }
 }
 
@@ -11,19 +10,17 @@ all_args_have_default_values <- function() {
   function(x) {
     args <- formals(x)
     args <- args[names(args) != "..."]
-    expectation(
+    expect_true(
       all(vapply(args, as.character, character(1L)) != ""),
-      "has arguments without default values",
-      "has only arguments with default values")
+      "has arguments without default values")
   }
 }
 
 has_method <- function(method_name) {
   function(x) {
     my_class <- class(x)
-    expectation(
+    expect_true(
       length(findMethod(method_name, my_class)) > 0L,
-      paste("object of class", my_class, "has no", method_name, "method"),
-      paste("object of class", my_class, "has a", method_name, "method"))
+      paste("object of class", my_class, "has no", method_name, "method"))
   }
 }
