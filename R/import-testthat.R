7f21b2afd27ec59dbd93f1085a159d274753005c
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2016-05-21 16:41:56 +0200

    import all of testthat

--- R/import-testthat.R
+++ R/import-testthat.R
@@ -1,14 +1,4 @@
-#' @importFrom testthat context
-#' @importFrom testthat expectation
-#' @importFrom testthat expect_equal
-#' @importFrom testthat expect_error expect_false
-#' @importFrom testthat expect_identical expect_is expect_s4_class
-#' @importFrom testthat expect_match
-#' @importFrom testthat expect_null
-#' @importFrom testthat expect_that expect_true expect_warning
-#' @importFrom testthat skip
-#' @importFrom testthat skip_on_appveyor skip_on_cran skip_on_travis
-#' @importFrom testthat test_that
+#' @import testthat
 NULL
 
 #' @importFrom methods findMethod getClasses getClass extends
