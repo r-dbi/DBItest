f3c2aeddfabcad8080bc9d219a1958b1020456e9
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2016-06-24 16:46:17 +0200

    widen intro

--- R/spec.R
+++ R/spec.R
@@ -1,7 +1,20 @@
 #' DBI specification
 #'
-#' This text formalizes the behavior expected by the functions declared in
-#' \code{DBI} and implemented by the individal backends..
+#' @description
+#' The \pkg{DBI} package defines the generic DataBase Interface for R.
+#' The connection to individual DBMS is made by packages that import \pkg{DBI}
+#' (so-called \emph{DBI backends}).
+#' This document formalizes the behavior expected by the functions declared in
+#' \pkg{DBI} and implemented by the individal backends.
+#'
+#' To ensure maximal portability and exchangeability, and to reduce the effort
+#' for implementing a new DBI backend, the \pkg{DBItest} package defines
+#' a comprehensive set of test cases that test conformance to the DBI
+#' specification.
+#' In fact, this document is derived from comments in the test definitions of
+#' the \pkg{DBItest} package.
+#' This ensures that an extension or update to the tests will be reflected in
+#' this document.
 #'
 #' @usage NULL
 #'
