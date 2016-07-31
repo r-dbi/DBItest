31de86a39b98d7226e913fa2d94d2b4ab92a7284
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2016-07-31 22:27:05 +0200

    move code

--- R/test-sql.R
+++ R/test-sql.R
@@ -15,20 +15,3 @@ test_sql <- function(skip = NULL, ctx = get_default_context()) {
 
   run_tests(ctx, spec_sql, skip, test_suite)
 }
-
-get_iris <- function(ctx) {
-  datasets_iris <- datasets::iris
-  if (isTRUE(ctx$tweaks$strict_identifier)) {
-    names(datasets_iris) <- gsub(".", "_", names(datasets_iris), fixed = TRUE)
-  }
-  datasets_iris
-}
-
-unrowname <- function(x) {
-  rownames(x) <- NULL
-  x
-}
-
-all_have_utf8_or_ascii_encoding <- function(x) {
-  all(vapply(x, has_utf8_or_ascii_encoding, logical(1L)))
-}
--- R/utils.R
+++ R/utils.R
@@ -1,1 +1,18 @@
 `%||%` <- function(a, b) if (is.null(a)) b else a
+
+get_iris <- function(ctx) {
+  datasets_iris <- datasets::iris
+  if (isTRUE(ctx$tweaks$strict_identifier)) {
+    names(datasets_iris) <- gsub(".", "_", names(datasets_iris), fixed = TRUE)
+  }
+  datasets_iris
+}
+
+unrowname <- function(x) {
+  rownames(x) <- NULL
+  x
+}
+
+all_have_utf8_or_ascii_encoding <- function(x) {
+  all(vapply(x, has_utf8_or_ascii_encoding, logical(1L)))
+}
