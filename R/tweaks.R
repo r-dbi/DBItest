1374d50d39e23209dd4cc5590f5772d905fea29c
Kirill Müller <krlmlr+r@mailbox.org>
2016-09-17 15:27:28 +0200

    oops

--- R/tweaks.R
+++ R/tweaks.R
@@ -39,7 +39,7 @@
     #'   resulting query returns the concatenated results of the subqueries
     "union",
 
-    #' @param placholder_pattern \code{[character]}\cr
+    #' @param placeholder_pattern \code{[character]}\cr
     #'   A pattern for placeholders used in \code{\link[DBI]{dbBind}}, e.g.,
     #'   \code{"?"}, \code{"$1"}, or \code{":name"}. See
     #'   \code{\link{make_placeholder_fun}} for details.
