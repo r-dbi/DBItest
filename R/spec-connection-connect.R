97170b8a6e21effa97cebc88a982588f3dc19444
Kirill Müller <krlmlr+r@mailbox.org>
2017-01-01 19:34:41 +0100

    spec dbConnect() and dbDisconnect()

--- R/spec-connection-connect.R
+++ R/spec-connection-connect.R
@@ -7,20 +7,36 @@ NULL
 #' @usage NULL
 #' @format NULL
 #' @keywords NULL
-#' @section Specification:
 spec_connection_connect <- list(
-  #' Can connect and disconnect, connection object inherits from
-  #'   "DBIConnection".
-  can_connect_and_disconnect = function(ctx) {
-    con <- connect(ctx)
-    expect_s4_class(con, "DBIConnection")
-    expect_true(dbDisconnect(con))
+  connect_and_disconnect_formals = function(ctx) {
+    # <establish formals of described functions>
+    expect_equal(names(formals(DBI::dbConnect)), c("drv", "..."))
+    expect_equal(names(formals(DBI::dbDisconnect)), c("conn", "..."))
   },
 
-  #' Repeated disconnect throws warning.
+  #' @return
+  can_connect_and_disconnect = function(ctx) {
+    con <- connect(ctx)
+    #' `dbConnect()` returns an S4 object that inherits from [DBIConnection-class].
+    expect_s4_class(con, "DBIConnection")
+    #'
+    #' `dbDisconnect()` returns `TRUE`, invisibly.
+    expect_true(expect_invisible(dbDisconnect(con)))
+  },
+
+  #' @section Specification:
+  cannot_disconnect_twice = function(ctx) {
+    expect_warning(gc(), NA)
+    connect(ctx)
+    #' A warning is issued when releasing a connection without calling
+    #' `dbDisconnect()`,
+    expect_warning(gc())
+  },
+
   cannot_disconnect_twice = function(ctx) {
     con <- connect(ctx)
     dbDisconnect(con)
+    #' or when calling `dbDisconnect()` on an already disconnected connection.
     expect_warning(dbDisconnect(con))
   },
 
