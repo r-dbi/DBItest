ea6e0ee9743f31c1a04e68bc4016b05aab3fb83d
Kirill Müller <krlmlr+r@mailbox.org>
2017-01-20 10:33:42 +0100

    oops

--- R/spec-driver-data-type.R
+++ R/spec-driver-data-type.R
@@ -99,11 +99,11 @@ test_data_type <- function(ctx, dbObj) {
   )
 
   #' The SQL data type for [factor]
-  expect_identical(dbDataType(con, letters),
-                   dbDataType(con, factor(letters)))
+  expect_identical(dbDataType(dbObj, letters),
+                   dbDataType(dbObj, factor(letters)))
   #' and [ordered] is the same as for character.
-  expect_identical(dbDataType(con, letters),
-                   dbDataType(con, ordered(letters)))
+  expect_identical(dbDataType(dbObj, letters),
+                   dbDataType(dbObj, ordered(letters)))
 
   #' The behavior for other object types is not specified.
 }
