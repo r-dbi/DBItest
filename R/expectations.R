8b5c01f1a97f2fb340058a7cfa53e6a01f26e33c
Kirill Müller <krlmlr+r@mailbox.org>
2017-01-31 23:24:54 +0100

    fix
    - Fix `expect_equal_df()` for list columns.

--- R/expectations.R
+++ R/expectations.R
@@ -47,17 +47,32 @@ expect_equal_df <- function(actual, expected) {
   factor_cols <- vapply(expected, is.factor, logical(1L))
   expected[factor_cols] <- lapply(expected[factor_cols], as.character)
 
-  order_actual <- do.call(order, actual)
-  order_expected <- do.call(order, expected)
+  asis_cols <- vapply(expected, inherits, "AsIs", FUN.VALUE = logical(1L))
+  expected[asis_cols] <- lapply(expected[asis_cols], unclass)
+
+  list_cols <- vapply(expected, is.list, logical(1L))
+
+  if (!any(list_cols)) {
+    order_actual <- order(actual)
+    order_expected <- order(expected)
+  } else {
+    expect_false(all(list_cols))
+    expect_equal(anyDuplicated(actual[!list_cols]), 0)
+    expect_equal(anyDuplicated(expected[!list_cols]), 0)
+    order_actual <- order(actual[!list_cols])
+    order_expected <- order(expected[!list_cols])
+  }
 
   has_rownames_actual <- is.character(attr(actual, "row.names"))
   has_rownames_expected <- is.character(attr(expected, "row.names"))
   expect_equal(has_rownames_actual, has_rownames_expected)
 
   if (has_rownames_actual) {
-    order_actual <- unrowname(order_actual)
-    order_expected <- unrowname(order_expected)
+    expect_equal(sort(row.names(actual)), sort(row.names(expected)))
   }
 
-  expect_identical(unrowname(actual[order_actual, ]), unrowname(expected[order_expected, ]))
+  actual <- unrowname(actual[order_actual, ])
+  expected <- unrowname(expected[order_expected, ])
+
+  expect_identical(actual, expected)
 }
