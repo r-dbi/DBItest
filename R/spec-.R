34dbaaf9e690d124a8991c858f3edae757ca309b
Kirill Müller <krlmlr+r@mailbox.org>
2017-02-26 21:45:04 +0100

    done

--- R/spec-.R
+++ R/spec-.R
@@ -9,6 +9,7 @@
 #
 # Output: Files R/test-xxx-1.R and R/test-xxx-2.R, and @include directives to stdout
 
+##### Stress
 #' @include spec-stress.R
 #' @include spec-stress-connection.R
 #' @include spec-stress-driver.R
