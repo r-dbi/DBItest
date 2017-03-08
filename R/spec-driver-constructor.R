dd2de6d16f8d24ee5665f52fd897f095e1b286e5
Kirill Müller <krlmlr+r@mailbox.org>
2017-03-08 16:01:59 +0100

    final changes before DBI release

--- R/spec-driver-constructor.R
+++ R/spec-driver-constructor.R
@@ -1,6 +1,6 @@
 #' @template dbispec-sub
 #' @format NULL
-#' @section Construction of the `DBIDriver` object:
+#' @section Construction of the DBIDriver object:
 spec_driver_constructor <- list(
   constructor = function(ctx) {
     pkg_name <- package_name(ctx)
@@ -12,11 +12,9 @@ spec_driver_constructor <- list(
     #' (if it exists), e.g., `SQLite` for the \pkg{RSQLite} package.
     default_constructor_name <- gsub("^R", "", pkg_name)
 
-    #' For the automated tests, the constructor name can be tweaked using the
-    #' `constructor_name` tweak.
+    #' However, backend authors may choose a different name.
     constructor_name <- ctx$tweaks$constructor_name %||% default_constructor_name
 
-    #'
     #' The constructor must be exported, and
     pkg_env <- getNamespace(pkg_name)
     eval(bquote(
@@ -28,17 +26,11 @@ spec_driver_constructor <- list(
     constructor <- get(constructor_name, mode = "function", pkg_env)
 
     #' that is callable without arguments.
-    #' For the automated tests, unless the
-    #' `constructor_relax_args` tweak is set to `TRUE`,
+    expect_that(constructor, all_args_have_default_values())
+    #' DBI recommends to define a constructor with an empty argument list.
     if (!isTRUE(ctx$tweaks$constructor_relax_args)) {
-      #' an empty argument list is expected.
       expect_that(constructor, arglist_is_empty())
-    } else {
-      #' Otherwise, an argument list where all arguments have default values
-      #' is also accepted.
-      expect_that(constructor, all_args_have_default_values())
     }
-    #'
   },
 
   NULL
