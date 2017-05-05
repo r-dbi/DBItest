d0559d58eb1349e9c40b67ed82f7b48897e08eb1
Kirill Müller <krlmlr+r@mailbox.org>
2017-05-05 14:22:57 +0200

    oops

--- R/spec-meta-bind.R
+++ R/spec-meta-bind.R
@@ -344,19 +344,19 @@ spec_meta_bind <- list(
     })
   },
 
-  #' - [POSIXct] timestamps
+  #' - [POSIXlt] timestamps
   bind_timestamp_lt = function(ctx) {
     if (!isTRUE(ctx$tweaks$timestamp_typed)) {
       skip("tweak: !timestamp_typed")
     }
 
     with_connection({
-      data_in <- as.POSIXct(round(Sys.time()))
+      data_in <- as.POSIXlt(round(Sys.time()))
       test_select_bind(
         con, ctx$tweaks$placeholder_pattern, data_in,
         type = dbDataType(con, data_in),
         transform_input = as.POSIXct,
-        transform_output = identity)
+        transform_output = as.POSIXct)
     })
   },
 
