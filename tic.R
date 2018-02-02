ef84aed785011ea8c1b44f23396b38ada78f8c7c
Kirill Müller <krlmlr+r@mailbox.org>
2018-02-02 08:39:05 +0100

    use tic

---
+++ tic.R
@@ -0,0 +1,13 @@
+get_stage("after_success") %>%
+  add_step(step_hello_world()) %>%
+  add_step(step_run_code(covr::codecov()))
+
+get_stage("deploy") %>%
+  add_step(step_install_ssh_keys()) %>%
+  add_step(step_test_ssh())
+
+if (ci()$get_branch() == "master" && Sys.getenv("BUILD_PKGDOWN") != "") {
+  get_stage("deploy") %>%
+    add_step(step_build_pkgdown()) %>%
+    add_step(step_push_deploy())
+}
