bc255df2da4d6eef86ff362721955f16085fba27
Kirill Müller <krlmlr+r@mailbox.org>
2018-04-23 22:38:14 +0200

    test dbIsReadOnly() again

--- R/spec-compliance-methods.R
+++ R/spec-compliance-methods.R
@@ -52,7 +52,7 @@ spec_compliance_methods <- list(
 
     dbi_generics <- grep("^[.]__T__db", getNamespaceExports(dbi), value = TRUE)
     . <- gsub("^[.]__T__(.*):DBI$", "\\1", dbi_generics)
-    . <- setdiff(., c("dbListConnections", "dbSetDataMappings", "dbGetException", "dbCallProc", "dbIsReadOnly"))
+    . <- setdiff(., c("dbListConnections", "dbSetDataMappings", "dbGetException", "dbCallProc"))
     dbi_names <- .
 
     exported_names <- callr::r(
