b27ae1a233203636fd9c2ef166ae785020dde784
Kirill Müller <krlmlr+r@mailbox.org>
2019-10-14 11:20:57 +0200

    Again

--- R/zzz.R
+++ R/zzz.R
@@ -1,7 +1,7 @@
 .onLoad <- function(libname, pkgname) {
   if (rlang::is_installed("debugme")) {
     # Necessary to re-parse environment variable
-    get(".onLoad", asNamespace("debugme"))(libname, pkgname)()
+    get(".onLoad", asNamespace("debugme"))(libname, pkgname)
 
     debugme::debugme()
   }
