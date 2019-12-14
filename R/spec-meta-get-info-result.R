ff74a2773a2d00a888746e1ac94f0739fd5b41df
Kirill Müller <krlmlr+r@mailbox.org>
2019-12-14 23:40:34 +0100

    Appease R 3.2?

--- R/spec-meta-get-info-result.R
+++ R/spec-meta-get-info-result.R
@@ -8,40 +8,38 @@ spec_meta_get_info_result <- list(
     #' @return
     #' For objects of class [DBIResult-class], `dbGetInfo()`
     with_connection({
-      with_result(
+      info <- with_result(
         dbSendQuery(con, trivial_query()),
-        {
-          info <- dbGetInfo(res)
-
-          #' returns a named list
-          expect_type(info, "list")
-
-          info_names <- names(info)
-
-          #' that contains at least the following components:
-          #'
-          necessary_names <- c(
-            #' - `statatment`: the statement used with [dbSendQuery()] or [dbExecute()],
-            #'   as returned by [dbGetStatement()],
-            "statement",
-            #' - `row.count`: the number of rows fetched so far (for queries),
-            #'   as returned by [dbGetRowCount()],
-            "row.count",
-            #' - `rows.affected`: the number of rows affected (for statements),
-            #'   as returned by [dbGetRowsAffected()]
-            "rows.affected",
-            #' - `has.completed`: a logical that indicates
-            #'   if the query or statement has completed,
-            #'   as returned by [dbHasCompleted()].
-            "has.completed"
-          )
-
-          for (name in necessary_names) {
-            eval(bquote(
-              expect_true(.(name) %in% info_names)))
-          }
-        }
+        info <- dbGetInfo(res)
       )
+
+      #' returns a named list
+      expect_type(info, "list")
+
+      info_names <- names(info)
+
+      #' that contains at least the following components:
+      #'
+      necessary_names <- c(
+        #' - `statatment`: the statement used with [dbSendQuery()] or [dbExecute()],
+        #'   as returned by [dbGetStatement()],
+        "statement",
+        #' - `row.count`: the number of rows fetched so far (for queries),
+        #'   as returned by [dbGetRowCount()],
+        "row.count",
+        #' - `rows.affected`: the number of rows affected (for statements),
+        #'   as returned by [dbGetRowsAffected()]
+        "rows.affected",
+        #' - `has.completed`: a logical that indicates
+        #'   if the query or statement has completed,
+        #'   as returned by [dbHasCompleted()].
+        "has.completed"
+      )
+
+      for (name in necessary_names) {
+        eval(bquote(
+          expect_true(.(name) %in% info_names)))
+      }
     })
   },
 
