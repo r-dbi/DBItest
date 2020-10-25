f070f46612cdc0f86a34d23e61af54baab83cfcd
Kirill Müller <krlmlr+r@mailbox.org>
2020-10-25 05:01:55 +0100

    R/spec-result-send-query.R

--- R/spec-result-send-query.R
+++ R/spec-result-send-query.R
@@ -107,14 +107,12 @@ spec_result_send_query <- list(
   },
 
   #' @inheritSection spec_result_get_query Specification for the `immediate` argument
-  send_query_immediate = function(con) {
-    with_remove_test_table({
+  send_query_immediate = function(con) with_remove_test_table({
       res <- expect_visible(dbSendQuery(con, trivial_query(), immediate = TRUE))
       expect_s4_class(res, "DBIResult")
       expect_error(dbGetRowsAffected(res), NA)
       dbClearResult(res)
-    })
-  },
+  }), # with_remove_test_table
   #
   NULL
 )
