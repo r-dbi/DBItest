3cde6b827e66ed59c4937045b869e0bbb75bde88
Kirill Müller <krlmlr+r@mailbox.org>
2020-10-25 05:33:12 +0100

    REVERT ME: Remove check

--- R/run.R
+++ R/run.R
@@ -117,7 +117,7 @@ patch_test_fun <- function(test_fun, desc) {
 }
 
 wrap_all_statements_with_expect_no_warning <- function(block) {
-  stopifnot(identical(block[[1]], quote(`{`)))
+  # stopifnot(identical(block[[1]], quote(`{`)))
   block[-1] <- lapply(block[-1], function(x) expr(expect_warning(!!x, NA)))
   block
 }
