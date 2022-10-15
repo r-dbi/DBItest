2297cd6d277fde5543f42a8066611e417ee59952
Kirill Müller <krlmlr+r@mailbox.org>
2022-10-15 18:55:15 +0200

    compat-purrr

--- R/import-testthat.R
+++ R/import-testthat.R
@@ -1,6 +1,7 @@
 #' @import testthat
 #' @importFrom rlang quo enquo enquos expr enexpr eval_tidy list2 has_length :=
-#' @importFrom rlang abort is_interactive
+#' @importFrom rlang abort is_interactive as_function local_options seq2 set_names
+#' @importFrom rlang %||% global_env is_logical
 NULL
 
 #' @importFrom methods findMethod getClasses getClass extends
