747b59e06c158c70ead81f5ca8191d1250def8b7
Kirill Müller <kirill@cynkra.com>
2023-11-08 10:30:39 +0100

    test: Fix checks without suggested packages

--- R/context.R
+++ R/context.R
@@ -23,7 +23,26 @@
 #' @rdname context
 #' @importFrom methods is new
 #' @export
-#' @example examples/make_context.R
+#' @examplesIf requireNamespace("RSQLite", quietly = TRUE)
+#' make_context(
+#'   new(
+#'     "DBIConnector",
+#'     .drv = RSQLite::SQLite(),
+#'     .conn_args = list(dbname = tempfile("DBItest", fileext = ".sqlite"))
+#'   ),
+#'   tweaks = tweaks(
+#'     constructor_relax_args = TRUE,
+#'     placeholder_pattern = c("?", "$1", "$name", ":name"),
+#'     date_cast = function(x) paste0("'", x, "'"),
+#'     time_cast = function(x) paste0("'", x, "'"),
+#'     timestamp_cast = function(x) paste0("'", x, "'"),
+#'     logical_return = function(x) as.integer(x),
+#'     date_typed = FALSE,
+#'     time_typed = FALSE,
+#'     timestamp_typed = FALSE
+#'   ),
+#'   default_skip = c("roundtrip_date", "roundtrip_timestamp")
+#' )
 make_context <- function(drv, connect_args = NULL, set_as_default = TRUE,
                          tweaks = NULL, name = NULL, default_skip = NULL) {
   if (is.null(drv)) {
