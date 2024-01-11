e11146b7bd2ccb5d04e99dd48e4683ebf4b9b82d
Kirill Müller <kirill@cynkra.com>
2024-01-11 17:42:31 +0100

    chore: Blanket rlang import

--- R/import-testthat.R
+++ R/import-testthat.R
@@ -1,17 +1,5 @@
-#' @import testthat
-#' @importFrom rlang quo enquo enquos expr enexpr eval_tidy list2 has_length :=
-#' @importFrom rlang abort is_interactive as_function local_options seq2 set_names
-#' @importFrom rlang %||% global_env is_logical is_installed quo_is_null
-#' @importFrom rlang call2
-#' @importFrom rlang caller_env
-#' @importFrom rlang exec
-#' @importFrom rlang pairlist2
-#' @importFrom rlang new_function
-#' @importFrom rlang check_dots_empty
-#' @importFrom rlang parse_expr
-#' @importFrom rlang quo_get_expr
-#' @importFrom rlang arg_match
-#' @importFrom rlang is_missing
+#' @rawNamespace import(testthat, except = c(is_null, is_false, is_true))
+#' @import rlang
 NULL
 
 #' @importFrom methods findMethod getClasses getClass extends
